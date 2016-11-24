ee93080299436a2eee10ec5b2fb4a68c9444e395
Hamza Kai <hamza@samsung.fritz.box>
2016-11-25 00:16:43 +0100

    added Ubuntu and raspberry seperation

--- main.cpp
+++ main.cpp
@@ -13,52 +13,61 @@ using namespace std;
 
 int main()
 {
-    namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
-    #ifndef LAPTOP
-    raspicam::RaspiCam_Cv Camera;
-    #endif
-    //Camera.set( CV_CAP_PROP_FRAME_HEIGHT, 240 );
-    //Camera.set( CV_CAP_PROP_FRAME_WIDTH, 320 );
+	//Open camera
+	cout<<"Opening Camera..."<<endl;
 
-    //Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 ); //schwarz weiß
-    #ifndef LAPTOP
-    Camera.set( CV_CAP_PROP_FORMAT, CV_8UC3);	//farbe
-    #endif
+#ifndef LAPTOP
+	raspicam::RaspiCam_Cv Camera;
+	//Camera.set( CV_CAP_PROP_FRAME_HEIGHT, 240 );
+	//Camera.set( CV_CAP_PROP_FRAME_WIDTH, 320 );
+	//Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 ); //schwarz weiß
+	Camera.set( CV_CAP_PROP_FORMAT, CV_8UC3);	//farbe
+	if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
 
-    //Open camera
-    cout<<"Opening Camera..."<<endl;
+#else
+	VideoCapture cap(0); // open the video camera no. 0
 
-    #ifndef LAPTOP
-    if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
-    #endif
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the video cam" << endl;
+		return -1;
+	}
 
-    while (1)
-    {
-        Mat frame;
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
 
-        //bool bSuccess = cap.read(frame); // read a new frame from video
-	#ifndef LAPTOP
-	Camera.grab();
-        Camera.retrieve (frame);
-	//cvtColor(frame, frame, CV_BGR2RGB);
-	cvtColor(frame, frame, CV_RGB2BGR);
-	//cv::cvtColor(bgrMat, rgbMat, cv::COLOR_BGR2RGB);
-	#endif
-        //if (!bSuccess) //if not success, break loop
-       // {
-       //      cout << "Cannot read a frame from video stream" << endl;
-       //      break;
-       // }
+	cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+#endif
+   
+	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+    
+	Mat frame;
 
-        imshow("MyVideo", frame); //show the frame in "MyVideo" window
+	while (1)
+	{
+		
 
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-       {
-            cout << "esc key is pressed by user" << endl;
-            break;
-       }
-    }
-    return 0;
-
+#ifndef LAPTOP
+		Camera.grab();
+		Camera.retrieve (frame);
+		cvtColor(frame, frame, CV_BGR2RGB);
+		//cvtColor(frame, frame, CV_RGB2BGR);
+		//cv::cvtColor(bgrMat, rgbMat, cv::COLOR_BGR2RGB);
+#else
+		bool bSuccess = cap.read(frame); // read a new frame from video
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+#endif
+		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+		if (waitKey(10) == 27) //wait for 'esc' key press for 30ms. If 'esc' ke$
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	cout << "A";
+	}
+	return 0;
 }
-
