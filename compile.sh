a42a12d9511f9bfe6c8cc2276acb767dab05685f
Hamza Ubuntu <ubuntu@ubuntu.de>
2016-12-03 14:12:45 +0100

    added some functions for better reading

--- compile.sh
+++ compile.sh
@@ -1,1 +1,4 @@
-g++ main.cpp -o  main -I/usr/local/include/ -lopencv_core -lopencv_highgui
+g++ -Wall -O2 main.cpp -o  main -I/usr/local/include/ -lopencv_core -lopencv_highgui
+
+#g++ -Wall -fexceptions -O2 -I/usr/local/include/opencv2 -I/usr/local/include/opencv -c main.cpp -o main.o
+#g++ -L/usr/local/bin -o main main.o  -s -lopencv_calib3d -lopencv_contrib -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_highgui -lopencv_imgproc -lopencv_legacy -lopencv_ml -lopencv_nonfree -lopencv_objdetect -lopencv_photo -lopencv_stitching -lopencv_superres -lopencv_ts -lopencv_video -lopencv_videostab  
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #define LAPTOP
 
+#define CAMERA_WIDTH   640
+#define CAMERA_HEIGHT  480
+
 #include <iostream>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <time.h>
 
 #ifndef LAPTOP
 #include <raspicam/raspicam_cv.h>
@@ -11,62 +15,141 @@
 using namespace cv;
 using namespace std;
 
+// Functions
+int initializeCamera();
+int getFrame();
+int getFps();
+
+//global variables
+Mat frame;
+#ifndef LAPTOP
+raspicam::RaspiCam_Cv Camera;
+#else
+VideoCapture camera(0);
+#endif
+
 int main()
 {
 	//Open camera
 	cout<<"Opening Camera..."<<endl;
+	initializeCamera();
+   
+	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+   
+ 
+     
+    while (1)
+	{
+        //getFrame();
+		camera >> frame;
+		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+		if (waitKey(1) == 27) //wait for 'esc' key press for 30ms. If 'esc' ke$
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}		
+	}
+	return 0;
+}
 
+/*
+####################################################################
+function to initialize the camera setup
+####################################################################
+*/
+int initializeCamera()
+{
 #ifndef LAPTOP
-	raspicam::RaspiCam_Cv Camera;
-	//Camera.set( CV_CAP_PROP_FRAME_HEIGHT, 240 );
-	//Camera.set( CV_CAP_PROP_FRAME_WIDTH, 320 );
+	//raspicam::RaspiCam_Cv Camera; now global variable
+	Camera.set( CV_CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH );
+	Camera.set( CV_CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT );
 	//Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 ); //schwarz weiß
 	Camera.set( CV_CAP_PROP_FORMAT, CV_8UC3);	//farbe
 	if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
 
 #else
-	VideoCapture cap(0); // open the video camera no. 0
+	//VideoCapture camera(0); // open the video camera no. 0 // now global variable
 
-	if (!cap.isOpened())  // if not success, exit program
+	if (!camera.isOpened())  // if not success, exit program
 	{
 		cout << "Cannot open the video cam" << endl;
 		return -1;
 	}
 
-	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+	camera.set(CV_CAP_PROP_FRAME_WIDTH,CAMERA_WIDTH);
+	camera.set(CV_CAP_PROP_FRAME_HEIGHT,CAMERA_HEIGHT);
 
-	cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+	//double dWidth = camera.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+	//double dHeight = camera.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+
+	//cout << "Frame size : " << dWidth << " x " << dHeight << endl;
 #endif
-   
-	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
-    
-	Mat frame;
+	return 0;
+}
 
-	while (1)
-	{
-		
 
+/*
+####################################################################
+function to get a frame from the camera
+####################################################################
+*/
+int getFrame()
+{
 #ifndef LAPTOP
-		Camera.grab();
-		Camera.retrieve (frame);
-		cvtColor(frame, frame, CV_BGR2RGB);
-		//cvtColor(frame, frame, CV_RGB2BGR);
-		//cv::cvtColor(bgrMat, rgbMat, cv::COLOR_BGR2RGB);
+	Camera.grab();
+	Camera.retrieve (frame);
+	cvtColor(frame, frame, CV_BGR2RGB);
+	//cvtColor(frame, frame, CV_RGB2BGR);
+	//cv::cvtColor(bgrMat, rgbMat, cv::COLOR_BGR2RGB);
 #else
-		bool bSuccess = cap.read(frame); // read a new frame from video
-		if (!bSuccess) //if not success, break loop
-		{
-			cout << "Cannot read a frame from video stream" << endl;
-			break;
-		}
-#endif
-		imshow("MyVideo", frame); //show the frame in "MyVideo" window
-		if (waitKey(10) == 27) //wait for 'esc' key press for 30ms. If 'esc' ke$
-		{
-			cout << "esc key is pressed by user" << endl;
-			break;
-		}
+	bool bSuccess = camera.read(frame); // read a new frame from video
+	if (!bSuccess) //if not success, break loop
+	{
+		cout << "Cannot read a frame from video stream" << endl;
+		//break;
 	}
+#endif
+	return 0;	
+}
+
+/*
+####################################################################
+function to get a frame from the camera
+####################################################################
+*/
+int getFps()
+{
+    double fps;
+    int num_frames = 300;
+    time_t start, end;
+
+	// Start time
+    time(&start);
+
+//	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+    // Grab a few frames
+    for(int i = 0; i < num_frames; i++)
+    {
+        //getFrame();
+		camera >> frame;
+//		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+//		if (waitKey(1) == 27) //wait for 'esc' key press for 30ms. If 'esc' ke$
+//		{
+//			cout << "esc key is pressed by user" << endl;
+//			break;
+//		}
+    }
+
+    // End Time
+    time(&end);
+
+	// Time elapsed
+    double seconds = difftime (end, start);
+    cout << "Time taken : " << seconds << " seconds" << endl;
+    
+    // Calculate frames per second
+    fps  = num_frames / seconds;
+    cout << "Estimated frames per second : " << fps << endl;
+
 	return 0;
 }
